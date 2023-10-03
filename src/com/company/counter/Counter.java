231c1951cc7fe832c4aa4faf8e11a4eb709d1d80
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2023-10-03 13:52:49 +0900

    Synchronized Counter run

--- src/com/company/counter/Counter.java
+++ src/com/company/counter/Counter.java
@@ -3,7 +3,7 @@ package com.company.counter;
 public class Counter {
     private int i = 0;
 
-    public int increment() {
+    public synchronized int increment() {
         return i = i + 1;
     }
 }
